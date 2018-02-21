1c61fb5002d4584122a13dacdde269ccad96d6a9
Vlad Ivanov <vlad211099@gmail.com>
2018-02-21 21:41:04 +0200

    implemented basic wrapping component

---
+++ app/components/Container/styles.js
@@ -0,0 +1,11 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    margin: 20,
+  },
+});
+
+export default styles;
