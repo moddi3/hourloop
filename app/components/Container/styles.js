b0b3c6cf71614796d3a48152fd4c2ba2920e876f
Vlad Ivanov <vlad211099@gmail.com>
2018-02-21 22:48:54 +0200

    fixed StatusBar margin issue

--- app/components/Container/styles.js
+++ app/components/Container/styles.js
@@ -1,10 +1,12 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { StatusBar } from 'react-native';
 
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     margin: 20,
+    marginTop: StatusBar.currentHeight,
   },
 });
 
