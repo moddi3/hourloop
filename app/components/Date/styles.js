452fd05c3bc691fd02276d2e396c7f75be113193
Vlad Ivanov <vlad211099@gmail.com>
2018-02-22 00:30:45 +0200

    implemented 'Date' component

---
+++ app/components/Date/styles.js
@@ -0,0 +1,11 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+  text: {
+    color: '#666',
+    fontSize: 10,
+    fontWeight: '400',
+  },
+});
+
+export default styles;
