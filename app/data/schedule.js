ac413a58fc74e0507e5e4cbd95d168ee97d20298
Vlad Ivanov <vlad211099@gmail.com>
2018-02-22 14:34:04 +0200

    sample data so far

---
+++ app/data/schedule.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    day: 'Monday',
+    classes: [
+      {
+        id: 0,
+        title: 'Algebra',
+        teacher: 'Danilova M V',
+        room: 302,
+        startsAt: '08:10 AM',
+        fav: false,
+        tasks: [],
+      },
+      {
+        id: 1,
+        title: 'English',
+        teacher: 'Smuschenko I A',
+        room: 302,
+        startsAt: '09:40 AM',
+        fav: false,
+        tasks: [],
+      },
+      {
+        id: 2,
+        title: 'Biology',
+        teacher: 'Medvid` S A',
+        room: 302,
+        startsAt: '11:20 AM',
+        fav: false,
+        tasks: [],
+      },
+      {
+        id: 3,
+        title: 'Chemistry',
+        teacher: 'Shokh V V',
+        room: 302,
+        startsAt: '12:50 PM',
+        fav: false,
+        tasks: [],
+      },
+    ],
+  },
+];
