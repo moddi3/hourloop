b08cd5bd6d46cc27cf6bdc6b5e33708816faa01e
Vlad Ivanov <vlad211099@gmail.com>
2018-02-26 21:50:20 +0200

    lesson card for schedule screen

---
+++ app/components/Card/styles.js
@@ -0,0 +1,23 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const styles = EStyleSheet.create({
+  container: {
+    marginBottom: 15,
+  },
+  card: {
+    // flexDirection: 'column',
+    // justifyContent: 'flex-start',
+    borderRadius: 2,
+    width: 260,
+    height: 110,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {},
+});
+
+export default styles;
