af54c9948bc03f6d4cf8aa606b9fb5f2bcb104c6
Vlad Ivanov <vlad211099@gmail.com>
2018-03-05 11:58:15 +0200

    added custom fonts loading

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Font } from 'expo';
+import { ActivityIndicator } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import Navigator from './config/routes';
@@ -9,6 +11,27 @@ EStyleSheet.build({
   $darkGray: '#333',
 });
 
-const App = () => <Navigator />;
+class App extends Component {
+  state = {
+    fontLoaded: false,
+  };
+  async componentWillMount() {
+    await Font.loadAsync({
+      'circular-regular': require('./assets/fonts/Circular-Book.otf'),
+      'circular-medium': require('./assets/fonts/Circular-Medium.otf'),
+      'circular-bold': require('./assets/fonts/Circular-Bold.otf'),
+      'circular-black': require('./assets/fonts/Circular-Black.otf'),
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    return this.state.fontLoaded ? (
+      <Navigator />
+    ) : (
+      <ActivityIndicator size="large" color="#3689E6" />
+    );
+  }
+}
 
 export default App;
