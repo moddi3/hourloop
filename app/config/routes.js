887a3ab270e22e4ebafe1f998a798abcfe0a0ac7
Vlad Ivanov <vlad211099@gmail.com>
2018-03-05 12:55:43 +0200

    each tab now has a header from StackNavigator

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,25 +1,68 @@
 import React from 'react';
-import { TabNavigator, TabBarBottom } from 'react-navigation';
+import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
 
 import Home from '../screens/Home';
 import Schedule from '../screens/Schedule';
+import More from '../screens/More';
+import Others from '../screens/Others';
 
-export default TabNavigator(
+import Header from '../components/Text';
+
+const headerStyle = { paddingLeft: 10 };
+
+const headerTitle = title => <Header fontSize={28}>{title}</Header>;
+
+const MoreStack = StackNavigator(
   {
-    Home: { screen: Home },
-    Schedule: { screen: Schedule },
+    More: {
+      screen: More,
+    },
+    Subjects: {
+      screen: props => <Others {...props} type="subjects" />,
+    },
+    Teachers: {
+      screen: props => <Others {...props} type="teachers" />,
+    },
   },
   {
     navigationOptions: ({ navigation }) => ({
+      headerTitle: headerTitle(navigation.state.routeName),
+      headerStyle,
+    }),
+  },
+);
+
+const Tabs = TabNavigator(
+  {
+    Home: { screen: Home },
+    Schedule: { screen: Schedule },
+    More: {
+      screen: MoreStack,
+      navigationOptions: {
+        header: null,
+      },
+    },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: headerTitle(navigation.state.routeName),
       tabBarIcon: ({ tintColor }) => {
         const { routeName } = navigation.state;
         let iconName;
-        if (routeName === 'Home') {
-          // iconName = `md-home${focused ? '' : '-outline'}`;
-          iconName = 'md-home';
-        } else if (routeName === 'Schedule') {
-          iconName = 'md-calendar';
+        switch (routeName) {
+          case 'Home':
+            iconName = 'md-home';
+            break;
+          case 'Schedule':
+            iconName = 'md-calendar';
+            break;
+          case 'More':
+            iconName = 'md-list-box';
+            break;
+          default:
+            iconName = 'logo-android';
+            break;
         }
         return <Ionicons name={iconName} size={25} color={tintColor} />;
       },
@@ -27,10 +70,11 @@ export default TabNavigator(
     tabBarPosition: 'bottom',
     tabBarComponent: TabBarBottom,
     swipeEnabled: false,
-    lazy: false,
+    animationEnabled: false,
     tabBarOptions: {
+      showLabel: false,
       activeTintColor: '#3689E6',
-      inactiveTintColor: '#666',
+      inactiveTintColor: '#999',
       pressColor: 'rgba(54, 137, 230, 0.2)',
       style: {
         backgroundColor: '#ffffff',
@@ -41,6 +85,17 @@ export default TabNavigator(
         fontSize: 10,
         marginBottom: 5,
       },
+    },
+  },
+);
+
+export default StackNavigator(
+  {
+    screen: Tabs,
+  },
+  {
+    navigationOptions: {
+      headerStyle,
     },
   },
 );
