3245126cd87e177ebea0bd00e2376ebeb6b0bb6d
Vlad Ivanov <vlad211099@gmail.com>
2018-03-16 16:57:42 +0200

    reworked for dynamic usage

--- app/components/Date/Date.js
+++ app/components/Date/Date.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Text } from 'react-native';
 import moment from 'moment';
 
 import styles from './styles';
 
 const Date = class Date extends Component {
-  state = {
-    date: null,
-  };
-
-  componentWillMount() {
-    const date = moment().format('MMMM D');
-    this.setState({ date });
-  }
+  state = {};
 
   render() {
-    const { date } = this.state;
+    const day = this.props.day ? this.props.day : moment().day();
+    const date = moment()
+      .day(day)
+      .format('MMMM D');
+
     return <Text style={styles.text}>{date.toUpperCase()}</Text>;
   }
 };
 
+Date.propTypes = {
+  day: PropTypes.number,
+};
 export default Date;
